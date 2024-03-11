f4dda35ee752def48cdf10892f20c4de15480096
electronyoon <electronyoon@gmail.com>
2024-03-11 12:26:43 +0900

    gitignore updated

--- test/test_concurrently.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Execute your script 10 times concurrently
-for i in {1..100}; do
-    ./test.sh &  # Execute your script in the background
-done
-
-# Wait for all background processes to finish
-wait
